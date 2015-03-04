7ffe9bb008326fd23d2dac5e3335c82230c38972
Carl Berg <carl@carl-berg.se>
2015-03-04 16:59:58 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Owin.Hosting;
+
+namespace WebCatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = args.Any() ? args[0] : "1234";
+            var uri = string.Format("http://localhost:{0}/", port);
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("\r\n");
+                Console.WriteLine("Web Catcher Started on port " + port + ". Press any key to exit...");
+                Console.ReadKey();
+                Console.WriteLine("Stopping Web Catcher");
+                Console.WriteLine("\r\n");
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+
+namespace WebCatcher
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                WriteRequest(context.Request);
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = 200;
+                return context.Response.WriteAsync("Web Catcher says Thank you!");
+            });
+        }
+
+        protected void WriteRequest(IOwinRequest request)
+        {
+            var defaultForegroundColor = Console.ForegroundColor;
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(request.Method);
+            Console.Write(" {0}\r\n", request.Uri.AbsoluteUri);
+            Console.ForegroundColor = defaultForegroundColor;
+        }
+    }
+}
